76b87ccfaddccefcb48e2bc9037bd81e6c283fef
LuisAxel <54085467+LuisAxel@users.noreply.github.com>
2022-06-05 13:06:31 -0500

    Add files via upload
    Secuencias utilizadas para la creación de pks artificiales

---
+++ s-05-secuencias.sql
@@ -0,0 +1,104 @@
+--@Autor: Zuriel Zárate García y Luis Axel Núñez Quintana
+--@Fecha creación:	03/06/2022
+--@Descripción:	Archivo de creación de secuencias
+
+connect zn_proy_admin/axzu
+
+create sequence avion_tmp_seq
+start with 1
+increment by 1
+maxvalue 1000
+minvalue 1
+cycle;
+
+create sequence avion_seq
+start with 1
+increment by 1
+maxvalue 1000
+minvalue 1
+nocycle;
+
+create sequence puesto_seq
+start with 1
+increment by 1
+maxvalue 100
+minvalue 1
+nocycle;
+
+create sequence empleado_seq
+start with 1
+increment by 1
+maxvalue 5000
+minvalue 1
+nocycle;
+
+create sequence aeropuerto_seq
+start with 1
+increment by 1
+maxvalue 500
+minvalue 1
+nocycle;
+
+create sequence vuelo_seq
+start with 1
+increment by 1
+maxvalue 10000
+minvalue 1
+nocycle;
+
+create sequence empleado_vuelo_seq
+start with 1
+increment by 1
+maxvalue 100000
+minvalue 1
+nocycle;
+
+create sequence pasajero_vuelo_seq
+start with 1
+increment by 1
+maxvalue 100000
+minvalue 1
+nocycle;
+
+create sequence pase_abordar_seq
+start with 1
+increment by 1
+maxvalue 100000
+minvalue 1
+nocycle;
+
+create sequence status_vuelo_seq
+start with 1
+increment by 1
+maxvalue 10
+minvalue 1
+nocycle;
+
+create sequence paquete_seq
+start with 1
+increment by 1
+maxvalue 100000
+minvalue 1
+nocycle;
+
+create sequence tipo_paquete_seq
+start with 1
+increment by 1
+maxvalue 100
+minvalue 1
+nocycle;
+
+create sequence historico_vuelo_seq
+start with 1
+increment by 1
+maxvalue 100000
+minvalue 1
+nocycle;
+
+create sequence pasajero_seq
+start with 1
+increment by 1
+maxvalue 10000
+minvalue 1
+nocycle;
+
