09bedce878aa620367bf13e19e184d4e3d07f2a3
LuisAxel <54085467+LuisAxel@users.noreply.github.com>
2022-06-05 22:51:00 -0500

    Update s-13-p-crea_avion.sql
    Ahora soporta excepciones producidas al insertar valores en las tablas de aviones.

--- s-13-p-crea_avion.sql
+++ s-13-p-crea_avion.sql
@@ -7,9 +7,24 @@ connect zn_proy_admin/axzu
 create or replace procedure p_crea_avion is
 
   v_existe number := 0;
-  v_antes number := 0;
-  v_despues number := 0;
   v_error varchar2(200);
+  
+  v_avion_id avion_tmp.avion_id%type;
+  v_matricula avion_tmp.matricula%type;
+  v_modelo avion_tmp.modelo%type;
+  v_especificaciones avion_tmp.especificaciones %type;
+  v_es_carga avion_tmp.es_carga%type;
+  v_es_comercial avion_tmp.es_comercial%type;
+  v_capacidad_ordinario avion_tmp.capacidad_ordinario%type;
+  v_capacidad_vip avion_tmp.capacidad_vip%type;
+  v_capacidad_discapacitado avion_tmp.capacidad_discapacitado%type;
+  v_bodega_profundidad avion_tmp.bodega_profundidad%type;
+  v_bodega_ancho avion_tmp.bodega_ancho%type;
+  v_bodega_alto avion_tmp.bodega_alto%type;
+  v_num_bodegas avion_tmp.num_bodegas%type;
+  v_capacidad avion_tmp.capacidad%type;
+  v_aeropuerto_id avion_tmp.aeropuerto_id%type;
+  
   cursor cur_avion is
     select avion_id, matricula, modelo, especificaciones, es_comercial, es_carga, capacidad_ordinario, 
            capacidad_discapacitado, capacidad_vip, bodega_profundidad, bodega_alto, bodega_ancho, num_bodegas,
@@ -21,6 +36,22 @@ begin
 
   for r in cur_avion loop
      
+    v_avion_id := r.avion_id;
+    v_matricula := r.matricula;
+    v_modelo := r.modelo;
+    v_especificaciones := r.especificaciones;
+    v_es_carga := r.es_carga;
+    v_es_comercial := r.es_comercial;
+    v_capacidad_ordinario := r.capacidad_ordinario;
+    v_capacidad_vip := r.capacidad_vip;
+    v_capacidad_discapacitado := r.capacidad_discapacitado;
+    v_bodega_profundidad := r.bodega_profundidad;
+    v_bodega_ancho := r.bodega_ancho;
+    v_bodega_alto := r.bodega_alto;
+    v_num_bodegas := r.num_bodegas;
+    v_capacidad := r.capacidad;
+    v_aeropuerto_id := r.aeropuerto_id;
+  
      v_error := 'Error(es):';
     
           
@@ -108,9 +139,21 @@ begin
           r.capacidad_discapacitado, r.capacidad_vip, r.bodega_profundidad, r.bodega_alto, r.bodega_ancho, r.num_bodegas,
           r.capacidad, r.aeropuerto_id, v_error);
     end if;
-    
   end loop;
-
+  
+  commit;
+    
+  exception
+    when others then
+      v_error := v_error || sqlerrm;
+      insert into avion_tmp_falla(avion_id, matricula, modelo, especificaciones, es_comercial, es_carga, capacidad_ordinario, 
+          capacidad_discapacitado, capacidad_vip, bodega_profundidad, bodega_alto, bodega_ancho, num_bodegas,
+          capacidad, aeropuerto_id,descripcion)
+        values(v_avion_id, v_matricula, v_modelo, v_especificaciones, v_es_comercial, v_es_carga, v_capacidad_ordinario, 
+          v_capacidad_discapacitado, v_capacidad_vip, v_bodega_profundidad, v_bodega_alto, v_bodega_ancho, v_num_bodegas,
+          v_capacidad, v_aeropuerto_id, v_error);
+    p_crea_avion();
+    
 end;
 /
 show errors
