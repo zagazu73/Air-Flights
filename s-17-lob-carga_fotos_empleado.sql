1c3f9ac9b413c7e6c59286aff7eefdcf4e4c93ac
LuisAxel <54085467+LuisAxel@users.noreply.github.com>
2022-06-07 03:58:11 -0500

    Add files via upload

---
+++ s-17-lob-carga_fotos_empleado.sql
@@ -0,0 +1,98 @@
+--@Autor: Zuriel Zárate García y Luis Axel Núñez Quintana
+--@Fecha creación:	27/05/2022
+--@Descripción:	Ejercicio 2, procedimiento para fotos de práctica 13
+
+prompt Creando objeto directorio
+connect sys/system as sysdba
+create or replace directory fotos_dir as '/tmp/Empleados';
+
+grant read on directory fotos_dir to zn_proy_admin;
+
+connect zn_proy_admin/axzu
+
+set serveroutput on
+
+create or replace procedure carga_fotos_empleado(
+  p_empleado_id in number, p_num_imagenes in number
+) is
+
+  cursor cur_fotos is
+    select empleado_id
+    from empleado
+    where empleado_id >= p_empleado_id and
+          empleado_id < p_empleado_id + p_num_imagenes;
+
+  v_bfile bfile;
+  v_foto blob;
+  v_dest_offset number;
+  v_src_offset number;
+  v_length_archivo number;
+  v_length_blob number;
+  ERROR_DE_CARGA exception;
+  pragma exception_init(ERROR_DE_CARGA,-20001);
+  
+  
+begin
+
+  dbms_output.put_line('Leyendo imágenes.');
+  for r in cur_fotos loop
+    v_src_offset := 1;
+    v_dest_offset := 1;
+    dbms_output.put_line('Procesando para empleado_id: ' || r.empleado_id);
+    -- Abriendo archivo
+    v_bfile := bfilename('FOTOS_DIR', 'empleado-'||r.empleado_id||'.jpg');
+    v_length_archivo := dbms_lob.getlength(v_bfile);
+    
+    -- Primero verifica si el archivo existe
+    if dbms_lob.fileexists(v_bfile) != 1 then
+      dbms_output.put_line('No se encontró el archivo: empleado-'||r.empleado_id||'.jgp');
+      continue;
+    end if;
+    
+    -- Segundo, asegurate que puede accederse a él
+    dbms_lob.fileopen(v_bfile, dbms_lob.lob_readonly);
+    if dbms_lob.isopen(v_bfile) != 1 then
+      dbms_output.put_line('No se encontró el archivo: empleado-'||r.empleado_id||'.jgp');
+      continue;
+    end if;
+    
+    -- Obteniendo referencia de la columna foto en modo exclusivo
+    -- Ningun usuario puede leer el contenido de la foto mientras se actualiza
+    select foto 
+    into v_foto
+    from empleado
+    where r.empleado_id = empleado_id
+    for update;  
+    
+    -- Cargando la foto en un solo paso
+    -- dbms_lob.loadblobfromfile(v_foto, v_bfile, v_length_archivo, v_src_offset, v_dest_offset);
+    dbms_lob.loadblobfromfile(
+      dest_lob    => v_foto, 
+      src_bfile   => v_bfile,
+      amount      => v_length_archivo,
+      src_offset  => v_src_offset,
+      dest_offset => v_dest_offset
+    );
+      
+    dbms_output.put_line('dest_offset = ' || v_dest_offset);  
+    dbms_output.put_line('Tamaño de foto = ' || v_length_archivo);  
+    
+    v_length_blob := dbms_lob.getlength(v_foto);
+    
+    dbms_lob.close(v_bfile);
+    
+    if v_length_blob != v_length_archivo then
+      dbms_output.put_line('Error: La foto no se cargó completamente, 
+      se esperaba: ' 
+      || v_length_archivo
+      || ' y se cargaron únicamente: '
+      || v_length_blob);
+      continue;
+    else
+      dbms_output.put_line('Carga exitosa.');
+    end if;
+  end loop;
+end;
+/
+show errors
+set serveroutput off
---
+++ s-18-lob-carga_fotos_empleado-prueba.sql
@@ -0,0 +1,58 @@
+--@Autor: Zuriel Zárate García y Luis Axel Núñez Quintana
+--@Fecha creación:	27/05/2022
+--@Descripción:	Ejercicio 2, procedimiento para probar el ejercicio 2 de la práctica 13
+
+
+connect zn_proy_admin/axzu
+
+set serveroutput on
+
+declare
+
+v_min number (3,0):= 1;
+v_max number (3,0):= 31;
+v_num_inicial number(3,0);
+v_num_fotos number(3,0);
+
+cursor cur_fotos is
+    select empleado_id, dbms_lob.getlength(foto) as longitud_foto
+    from empleado;
+
+
+ARCHIVO_NO_ENCONTRADO exception;
+pragma exception_init(ARCHIVO_NO_ENCONTRADO,-22288);
+ 
+begin
+    dbms_output.put_line(chr(5));
+    dbms_output.put_line('===========================================================');
+    dbms_output.put_line('Escenario 1- Todas las imagenes se actualizan correctamente');
+    dbms_output.put_line('===========================================================');
+  
+    v_num_inicial := 1;
+    v_num_fotos := 30;
+    if v_num_inicial < v_min or v_num_inicial + v_num_fotos > v_max then
+      dbms_output.put_line('Valor de parametros fuera de los limites!!!');
+      dbms_output.put_line('Los valores deben estar dentro del rango ['||v_min||','||v_max||']');
+    else
+      carga_fotos_empleado(v_num_inicial,v_num_fotos);
+      dbms_output.put_line('Todas las imagenes fueron actualizadas =========> OK! Escenario 1 correcto');
+    end if;
+   
+    dbms_output.put_line('Mostrando la longitud de los archivos cargados...');
+    for r in cur_fotos loop
+      dbms_output.put_line(r.empleado_id || ' ' || r.longitud_foto);
+    end loop;
+    
+  exception 
+    when ARCHIVO_NO_ENCONTRADO then
+      dbms_output.put_line('No se encontró el archivo! Verifique que el nombre del archivo es: empleado-<empleado_id>.jpg');
+      dbms_output.put_line('');
+      dbms_output.put_line('');
+      dbms_output.put_line('Error en la prueba...=========> Escenario 1 incorrecto');
+    when others then
+      raise;
+end;
+/
+
+Prompt Haciendo rollback para limpiar tablas
+set serveroutput off
